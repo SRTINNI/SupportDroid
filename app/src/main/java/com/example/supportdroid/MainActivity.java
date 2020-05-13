347542997c1e30ac6163c58226b4f7d66dd4beaa
SRTINNI <srtinni@gmail.com>
2020-05-13 13:20:36 +0600

    Front page design

---
+++ app/src/main/java/com/example/supportdroid/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.supportdroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //variables
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.aajpg);
+        logo = findViewById(R.id.text1);
+        slogan = findViewById(R.id.text2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation((bottomAnim));
+
+
+    }
+}
